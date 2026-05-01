6d7b9f822f8665c194b0ec59432edc2a1d86fb01
FrancisOWO <francisowo@163.com>
2026-05-01 18:36:45 +0800

    fix: 页面空白问题

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
                 target: 'http://127.0.0.1:3000',
                 changeOrigin: true,
             },
-            '/ai': {
+            '/ai/': {
                 target: 'http://127.0.0.1:3000',
                 changeOrigin: true,
             },
